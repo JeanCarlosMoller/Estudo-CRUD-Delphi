43fcba4600d955b6574ba8fc7a12dda9e19e95e0
Jean Carlos Möller <jeancmoller17@gmail.com>
2023-01-10 01:03:19 -0300

    alteração do banco de dados para sqllite. Criptografia de senha. Reconfiguração de telas e pastas de projeto. Essas melhorias e ideias foram aprendidas no curso do clube de programadores delphi

---
+++ src/Model/__history/MonolitoFinanceiro.Model.Conexao.dfm.~16~
@@ -0,0 +1,17 @@
+object dmConexao: TdmConexao
+  OldCreateOrder = False
+  OnCreate = DataModuleCreate
+  Height = 426
+  Width = 561
+  object FDQuery1: TFDQuery
+    Connection = SQLConexao
+    SQL.Strings = (
+      'Select * from usuarios')
+    Left = 40
+    Top = 112
+  end
+  object SQLConexao: TFDConnection
+    Left = 72
+    Top = 56
+  end
+end
---
+++ src/View/SistemaMoller.View.Principal.pas
@@ -0,0 +1,83 @@
+unit SistemaMoller.View.Principal;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.Menus,
+  SistemaMoller.View.Usuarios, Vcl.ComCtrls, Vcl.ExtCtrls, Vcl.Imaging.pngimage;
+
+type
+  TfrmPrincipal = class(TForm)
+    MainMenu1: TMainMenu;
+    mnuCadastro: TMenuItem;
+    mnuSobre: TMenuItem;
+    mnuUsuarios: TMenuItem;
+    StatusBar1: TStatusBar;
+    Timer1: TTimer;
+    Image1: TImage;
+    procedure mnuUsuariosClick(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
+    procedure Timer1Timer(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  frmPrincipal: TfrmPrincipal;
+
+implementation
+
+uses
+  SistemaMoller.View.Splash,
+  SistemaMoller.View.Login,
+  SistemaMoller.Model.Usuarios, SistemaMoller.View.RedefinirSenha;
+
+{$R *.dfm}
+
+procedure TfrmPrincipal.FormCreate(Sender: TObject);
+begin
+  frmSplash := TFrmSplash.Create(nil);
+  try
+    frmSplash.ShowModal;
+  finally
+    FreeAndNil(frmSplash);
+  end;
+  frmLogin := TFrmLogin.Create(nil);
+  try
+    frmLogin.ShowModal;
+    if frmLogin.ModalResult <> mrOk then
+      Application.Terminate;
+  finally
+    FreeAndNil(frmLogin);
+  end;
+  if dmUsuarios.GetUsuarioLogado.SenhaTemporaria then
+  begin
+    frmRedefinirSenha :=  TfrmRedefinirSenha.Create(nil);
+    try
+      frmRedefinirSenha.Usuario := dmUsuarios.GetUsuarioLogado;
+      frmRedefinirSenha.ShowModal;
+      if frmRedefinirSenha.ModalResult <> mrOK then
+        Application.Terminate;
+    finally
+      FreeAndNil(frmRedefinirSenha);
+
+    end;
+  end;
+
+  StatusBar1.Panels.Items[1].Text := 'Usuário: ' + dmUsuarios.GetUsuarioLogado.Nome;
+end;
+
+procedure TfrmPrincipal.mnuUsuariosClick(Sender: TObject);
+begin
+  frmUsuarios.Show;
+end;
+
+procedure TfrmPrincipal.Timer1Timer(Sender: TObject);
+begin
+  StatusBar1.Panels.Items[0].Text := DateTimeToStr(Now);
+end;
+
+end.
